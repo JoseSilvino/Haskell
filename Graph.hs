3be3feda4ba16e479dd7e4c7e70677021db6b2c1
JoseSilvino <43120521+JoseSilvino@users.noreply.github.com>
2019-03-08 11:04:27 -0300

    Add files via upload

--- Graph.hs
+++ Graph.hs
@@ -1,16 +1,34 @@
-pertence :: Integer ->[Integer]-> Bool
+pertence :: (Eq a) =>a ->[a]-> Bool
 pertence x [] = False
 pertence x (a:as)
       |(x == a) = True
       |otherwise = pertence x as
-eGrafo :: [Integer] -> [(Integer,Integer)] -> Bool
+eGrafo :: (Eq a)=>[a] -> [(a,a)] -> Bool
 eGrafo [] _ = False
 eGrafo v [] = True
 eGrafo v ((o,d):as) = o `pertence` v && d `pertence` v && eGrafo v as
 
-grau:: Integer->[(Integer,Integer)]->Integer
+grau:: (Eq a) =>a->[(a,a)]->Integer
 grau v [] = 0
 grau v ((x,y):as) 
                  | v == x = if(v==y) then 2 + grau v as else 1 + grau v as
-                 | v == y && not(v==x) = 1 + grau v as
+                 | v == y  = 1 + grau v as
                  | otherwise = grau v as
+
+grauEmissao :: (Eq a) =>a -> [(a,a)] ->Integer
+grauEmissao v [] = 0
+grauEmissao v ((x,y):as) 
+               |v == x = 1 + grauEmissao v as
+               |otherwise = grauEmissao v as
+
+grauRecepcao :: (Eq a) => a -> [(a,a)] -> Integer
+grauRecepcao v [] = 0
+grauRecepcao v ((x,y):as) 
+                         |v == y = 1 + grauRecepcao v as
+                         | otherwise = grauRecepcao v as
+                         
+sumidouro :: (Eq a ) => a -> [(a,a)] -> [Char]
+sumidouro v ((o,d):as)
+                      |grauEmissao v ((o,d):as) ==0 && grauRecepcao v((o,d):as) /= 0 = "Eh sumidouro"
+                      | grauEmissao v ((o,d):as) /= 0 && grauRecepcao v ((o,d):as) == 0 = "Eh fonte"
+                      |otherwise ="Nao eh nada"
